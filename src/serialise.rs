2ded48d549e34a79ee0a51e048c9ac76d9c7e6e1
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-03-17 22:33:59 +0000

    serialise: Add Sized to trait, Option<self>
    The NetSerial trait is now Sized and returns Option<Self> when
    deserialising bytes into a packet.
    
    u8_push is now a part of the serialise module as a helper function
    for adding a slice of [u8] to a Vec<u8>

--- src/serialise.rs
+++ src/serialise.rs
@@ -1,4 +1,10 @@
-pub trait NetSerial {
+pub trait NetSerial : Sized {
 	fn serialise(&self) -> Vec<u8>;
-	//fn deserialise(bytes: [u8]) -> Self;
+	fn deserialise(bytes: &[u8]) -> Option<Self>;
+}
+
+pub fn push_bytes(v: &mut Vec<u8>, bytes: &[u8]) {
+	for b in bytes {
+		v.push(*b)
+	}
 }
