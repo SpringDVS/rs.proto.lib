484c72b2ae18848bde286e9aaf3b4006cbbe8f9a
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-03-18 09:39:20 +0000

    serialise: Move in transmute and array slicing
    - These transmute functions handle transmuting memory from serialised
      byte arrays into primitives.
    
    - Include the array slice into 4 byte array conversion which will be
      useful for handling IPv4

--- src/serialise.rs
+++ src/serialise.rs
@@ -1,3 +1,5 @@
+use std::mem::transmute;
+
 pub trait NetSerial : Sized {
 	fn serialise(&self) -> Vec<u8>;
 	fn deserialise(bytes: &[u8]) -> Option<Self>;
@@ -7,4 +9,17 @@ pub fn push_bytes(v: &mut Vec<u8>, bytes: &[u8]) {
 	for b in bytes {
 		v.push(*b)
 	}
+}
+
+pub fn u32_transmute_be_arr(a: &[u8]) -> u32 {
+	unsafe { transmute::<[u8;4], u32>([a[3], a[2], a[1], a[0]]) } 
+}
+
+pub fn u32_transmute_le_arr(a: &[u8]) -> u32 {
+	unsafe { transmute::<[u8;4], u32>([a[0], a[1], a[2], a[3]]) } 
+}
+
+
+pub fn byte_slice_4array(a: &[u8]) -> [u8;4] {
+	[ a[0], a[1], a[2], a[3] ]
 }
