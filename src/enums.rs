5e5d5b05a57f466c12c163274d8f9cd6b70367b8
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-03-19 14:45:14 +0000

    licensing: Add license headers

--- src/enums.rs
+++ src/enums.rs
@@ -1,3 +1,7 @@
+/* Notice:	Copyright 2016, The Care Connections Initiative c.i.c.
+ * Author: 	Charlie Fyvie-Gauld (cfg@zunautica.org)
+ * License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+ */
 // ----- Enumeration Lists ----- \\
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum DvspMsgType {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+/* Notice:	Copyright 2016, The Care Connections Initiative c.i.c.
+ * Author: 	Charlie Fyvie-Gauld (cfg@zunautica.org)
+ * License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+ */
 extern crate spring_dvs;
 
 fn main() {
