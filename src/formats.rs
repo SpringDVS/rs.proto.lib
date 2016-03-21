07f67d0ffcae7fc8dbdbd3b2aed3491fe66d6c1c
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-03-21 14:19:28 +0000

    formats: Add string to Ipv4 conversion function and tests
    The conversion function includes fail states for invalid address
    strings. The function passes tests

--- src/formats.rs
+++ src/formats.rs
@@ -3,3 +3,25 @@
  * License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
+use ::protocol::Ipv4;
+use ::enums::Failure;
+
+pub fn str_address_to_ipv4(address: &str) -> Result<Ipv4, Failure> {
+	let atom: Vec<&str> = address.split('.').collect();
+	
+	if atom.len() != 4 {
+		return Err(Failure::InvalidFormat);
+	};
+		
+	let mut addr: Ipv4 = [0;4];
+	
+	for i in 0..4 {
+		
+		addr[i] = match atom[i].parse::<u32>().unwrap() {
+			v if v < 0xFF  => v,
+			_ => return Err(Failure::InvalidBytes)
+		} as u8;
+	}
+
+	Ok(addr)
+}
---
+++ tests/testing_formats.rs
@@ -0,0 +1,37 @@
+/* Notice:  Copyright 2016, The Care Connections Initiative c.i.c.
+ * Author:  Charlie Fyvie-Gauld (cfg@zunautica.org)
+ * License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+extern crate spring_dvs;
+use spring_dvs::formats::str_address_to_ipv4;
+use spring_dvs::enums::Failure;
+#[test]
+fn ts_formats_str_address_to_ipv4_p() {
+	
+	// Test pass
+	let straddr = "192.168.1.2";
+	let r = str_address_to_ipv4(straddr);
+	
+	assert!(r.is_ok());
+	
+	assert_eq!([192,168,1,2], r.unwrap())
+}
+
+#[test]
+fn ts_formats_str_address_to_ipv4_f() {
+	
+	// Test fail
+	
+	// Invalid format
+	let straddr1 = "192.168.1";
+	let r1 = str_address_to_ipv4(straddr1);
+	assert!(r1.is_err());
+	assert_eq!(Failure::InvalidFormat, r1.unwrap_err());
+	
+	// Invalid bytes
+	let straddr2 = "192.168.1.384";
+	let r2 = str_address_to_ipv4(straddr2);
+	assert!(r2.is_err());
+	assert_eq!(Failure::InvalidBytes, r2.unwrap_err());
+}
+
