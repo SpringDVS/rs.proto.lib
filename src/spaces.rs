b3f954f7a3a1cc8ccc8cb4dff7908546885fba12
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-06-14 16:00:04 +0100

    netspace: Add specific failure enum

--- src/spaces.rs
+++ src/spaces.rs
@@ -5,8 +5,14 @@
 
 pub use ::protocol;
 pub use ::node::Node;
-use ::enums::{Failure,Success,NodeState,NodeRole};
+use ::enums::{Success,NodeState,NodeRole};
 
+#[derive(Debug,PartialEq)]
+pub enum NetspaceFailure {
+	NodeNotFound,
+	DuplicateNode,
+	DatabaseError,
+}
 
 
 pub trait Netspace {
@@ -15,25 +21,25 @@ pub trait Netspace {
 	fn gsn_nodes_by_type(&self, types: NodeRole) -> Vec<Node>;
 	fn gsn_nodes_by_state(&self, state: NodeState) -> Vec<Node>;
 	
-	fn gsn_node_by_springname(&self, name: &str) -> Result<Node,Failure>;
-	fn gsn_node_by_hostname(&self, name: &str) -> Result<Node,Failure>;
+	fn gsn_node_by_springname(&self, name: &str) -> Result<Node,NetspaceFailure>;
+	fn gsn_node_by_hostname(&self, name: &str) -> Result<Node,NetspaceFailure>;
 	
 	
 	fn gtn_root_nodes(&self) -> Vec<Node>;
 	fn gtn_geosubs(&self) -> Vec<String>;
 	
-	fn gsn_node_register(&self, node: &Node) -> Result<Success,Failure>;
-	fn gsn_node_unregister(&self, node: &Node) -> Result<Success,Failure>;
+	fn gsn_node_register(&self, node: &Node) -> Result<Success,NetspaceFailure>;
+	fn gsn_node_unregister(&self, node: &Node) -> Result<Success,NetspaceFailure>;
 
-	fn gsn_node_update_state(&self, node: &Node) -> Result<Success,Failure>;
-	fn gsn_node_update_service(&self, node: &Node) -> Result<Success,Failure>;
-	fn gsn_node_update_role(&self, node: &Node) -> Result<Success,Failure>;
+	fn gsn_node_update_state(&self, node: &Node) -> Result<Success,NetspaceFailure>;
+	fn gsn_node_update_service(&self, node: &Node) -> Result<Success,NetspaceFailure>;
+	fn gsn_node_update_role(&self, node: &Node) -> Result<Success,NetspaceFailure>;
 	
 	fn gtn_geosub_root_nodes(&self, gsn: &str) -> Vec<Node>;
-	fn gtn_geosub_node_by_springname(&self, name: &str, gsn: &str) -> Result<Node,Failure>;
+	fn gtn_geosub_node_by_springname(&self, name: &str, gsn: &str) -> Result<Node,NetspaceFailure>;
 	 
-	fn gtn_geosub_register_node(&self, node: &Node, gsn: &str) -> Result<Success,Failure>;
-	fn gtn_geosub_unregister_node(&self, node: &Node, gsn: &str) -> Result<Success,Failure>;
+	fn gtn_geosub_register_node(&self, node: &Node, gsn: &str) -> Result<Success,NetspaceFailure>;
+	fn gtn_geosub_unregister_node(&self, node: &Node, gsn: &str) -> Result<Success,NetspaceFailure>;
 	
 }
 
